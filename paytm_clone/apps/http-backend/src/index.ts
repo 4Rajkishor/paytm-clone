694069e9c0326ad6960bdf41cb98679a080ef9e6
Raj Upadhyay <raj.upadhyay3@gmail.com>
2026-05-26 23:40:15 +0530

    fix: add tsx for common package ts support

--- paytm_clone/apps/http-backend/src/index.ts
+++ paytm_clone/apps/http-backend/src/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import {SigninSchema,SignupSchema} from "@repo/common/schema"
+import {SigninSchema,SignupSchema} from "@repo/common/types"
 import bcrypt from "bcrypt";
 import{prismaClient} from "@repo/db/dbSchema"
 const app= express();
--- paytm_clone/packages/common/src/index.ts
+++ paytm_clone/packages/common/src/index.ts
@@ -1,4 +1,4 @@
-import z, { email } from "zod";
+import  { z } from "zod";
 
 export const SignupSchema=z.object({
   username:z.string().max(15),
